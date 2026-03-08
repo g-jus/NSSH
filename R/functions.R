2fa6cfa77c5e35c4dcdbb8b216dc80187082a59d
g-jus <gusun5199@uib.no>
2026-03-08 17:53:48 +0100

    import data function

--- R/functions.R
+++ R/functions.R
@@ -5,3 +5,10 @@
 #' @return A `data.frame`
 #' @export
 
+get_herring_data <- function() {
+  url <- "https://ftp.nmdc.no/nmdc/IMR/Herring/HerringData.csv"
+  tmp <- tempfile(fileext = ".csv")
+  download.file(url, tmp, mode = "wb")
+  data <- read.csv(tmp)
+  return(data)
+}
